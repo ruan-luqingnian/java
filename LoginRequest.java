66a696b6714dd77985f3ea3e1b9a20d06a3bcbd4
ruan-luqingnian <80663588+ruan-luqingnian@users.noreply.github.com>
2021-04-14 07:27:40 +0800

    Add files via upload

---
+++ LoginRequest.java
@@ -0,0 +1,27 @@
+package edu.XUFE.online_class.model.request;
+
+/**
+ * 登录Request
+ */
+public class LoginRequest {
+
+    private String phone;
+
+    private String pwd;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
